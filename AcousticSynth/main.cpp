bdce252d0bfe8bd6112273fb60adcd56a2b347fb
池田央 <g1444062@gmail.com>
2016-07-27 17:23:40 +0900

    Initial Commit

---
+++ AcousticSynth/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AcousticSynth
+//
+//  Created by 池田央 on 2016/07/27.
+//  Copyright © 2016年 池田央. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
